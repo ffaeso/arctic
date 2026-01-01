9a237399d3ccd31351c4ce50967d6f63c071f610
Austin Sofaer <164747892+ffaeso@users.noreply.github.com>
2026-01-02 02:48:14 +1100

    feat: simple healthcheck + route group

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,22 +2,30 @@ package server
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httplog/v3"
 )
 
-func GetRoutes() http.Handler {
+func Mount(logger *slog.Logger) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
+	r.Use(httplog.RequestLogger(logger, nil))
 	r.Use(middleware.Recoverer)
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(map[string]string{"status": "up"})
+	})
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Use(middleware.SetHeader("Content-Type", "application/json"))
 	})
 
 	return r
